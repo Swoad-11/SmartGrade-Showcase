7305993ddb173c89e90a84e419af74d516320e6a
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-01-11 13:49:07 +0600

    hero section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -5,6 +6,7 @@ export default function App() {
     <>
       <body className="bg-[#172227] font-[Inter] text-white">
         <Navbar />
+        <Hero />
       </body>
     </>
   );
