52cb53ebf2d387f5e8026ed09c5e22d472df1e56
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-01-11 17:36:32 +0600

    footer added, all requirements fulfilled

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import StudentsTable from "./components/StudentsTable/StudentsTable";
@@ -9,6 +10,7 @@ export default function App() {
         <Navbar />
         <Hero />
         <StudentsTable />
+        <Footer />
       </body>
     </>
   );
